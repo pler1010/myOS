a2db342251bbbb3c51990d71970a5140ca2e9834
wenyuwei <wenyuwei925@gmail.com>
2024-09-24 18:59:25 +0800

    Complete LAB1 EXERCISE2

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -112,6 +112,14 @@ void interrupt_handler(struct trapframe *tf) {
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
+            clock_set_next_event();
+            num++;   
+            if (num % TICK_NUM == 0) {
+                print_ticks();
+                if (num / TICK_NUM >= 10) {
+                    shut_down(); 
+                }
+            }
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
